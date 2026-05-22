9151791da042add0c1070d145a0a1b1ee15fc7cc
Amiithesh Dhanuskodi <amiithesh.dhanuskodi29@gmail.com>
2026-05-22 13:08:27 -0500

    Initial commit

---
+++ Run.py
@@ -0,0 +1,14 @@
+class Run:
+    
+    def __init__(self, distance, pace, heart_rate, date):
+        self.distance = distance
+        self.pace = pace
+        self.heart_rate = heart_rate
+        self.date = date
+        
+        
+        
+        
+test = Run(1,2,4,5)
+
+print(test.distance)
