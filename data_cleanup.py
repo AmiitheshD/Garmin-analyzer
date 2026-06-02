7e8ace22b70ac72df77e98aadd2067f41397ddb4
Amiithesh Dhanuskodi <amiithesh.dhanuskodi29@gmail.com>
2026-06-02 16:56:17 -0500

    Added new comments and the turned the data frame into a list of run classes

--- data_cleanup.py
+++ data_cleanup.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import Run
 data_frame = pd.read_csv("garmin_data.csv")
 data_frame.columns = data_frame.columns.str.strip()
 cols_to_keep = [
@@ -17,18 +17,31 @@ to_drop = [i for i in data_frame.columns if i not in cols_to_keep]
 
 data_frame.drop(columns=to_drop, inplace=True)
 
-data_frame.info()
 
 data_frame = data_frame.rename(columns={
     "Avg HR": "avg_hr",
-    "Avg Pace": "avg_pace",
     "Total Ascent": "total_ascent",
     "Total Descent": "total_descent",
     "Avg Power": "avg_power",
     "Distance": "distance",
-    "Date": "date"
+    "Date": "date",
+    "Moving Time" : "moving_time"
     
 })
+
+runs = []
+for _, row in data_frame.iterrows():
+    runs.append(Run.Run(
+        date=row["date"],
+        distance=row["distance"],
+        moving_time=row["moving_time"],
+        avg_hr=row["avg_hr"],
+        total_ascent=row["total_ascent"],
+        total_descent=row["total_descent"],
+        avg_power=row["avg_power"],
+    ))
+    
+    print(runs)
 
 
 
