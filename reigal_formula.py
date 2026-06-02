4459eedd01306077aa95b25862360f6a77852842
Amiithesh Dhanuskodi <amiithesh.dhanuskodi29@gmail.com>
2026-06-02 16:30:27 -0500

    added the basic format of the regial formula with the default expoenent

--- reigal_formula.py
+++ reigal_formula.py
@@ -1,1 +1,9 @@
 import data_cleanup
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+RIEGEL_DEFAULT = 1.06
+
+def riegel_predict(known_distance, known_time_hrs, target_distance, exponent=RIEGEL_DEFAULT):
+    """Predict finish time (hours) for target_distance given a known effort."""
+    return known_time_hrs * (target_distance / known_distance) ** exponent
