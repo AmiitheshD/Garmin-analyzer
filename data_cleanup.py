d9d7b1cb57ce1d183928a31f813cfdb116fb8de6
Amiithesh Dhanuskodi <amiithesh.dhanuskodi29@gmail.com>
2026-06-05 11:12:52 -0500

    Included max heart rate in the data frame

--- data_cleanup.py
+++ data_cleanup.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import Run
+##Turns the csv file into a data frame
 data_frame = pd.read_csv("garmin_data.csv")
 data_frame.columns = data_frame.columns.str.strip()
+
+##These are the only potential data coloumns we would need for the sake of the two computer models
 cols_to_keep = [
     "Date",
     "Distance",
@@ -9,15 +12,16 @@ cols_to_keep = [
     "Avg HR",
     "Total Ascent",
     "Total Descent",
-    "Avg Power" # optional, drop if not using
+    "Avg Power",# optional, drop if not using
+    "Max HR"
 ]
-
+#Filtering out the data frame
 to_drop = [i for i in data_frame.columns if i not in cols_to_keep]
 
 
 data_frame.drop(columns=to_drop, inplace=True)
 
-
+#renaming the coloumns to be more compatable with the RUN classs
 data_frame = data_frame.rename(columns={
     "Avg HR": "avg_hr",
     "Total Ascent": "total_ascent",
@@ -25,10 +29,12 @@ data_frame = data_frame.rename(columns={
     "Avg Power": "avg_power",
     "Distance": "distance",
     "Date": "date",
-    "Moving Time" : "moving_time"
+    "Moving Time" : "moving_time",
+    "Max HR": "max_hr"
     
 })
 
+#Turning the data frame into a list of Run classes
 runs = []
 for _, row in data_frame.iterrows():
     runs.append(Run.Run(
